a50ae2d13f4a813385707ba0a1cdf200d35067d5
cszalai <cszalai@davincisoftware.sk>
2017-02-09 17:37:32 +0100

    morer changes for chapter eight (selection-decision)

--- src/test/java/com/javafortesters/chap008/examples/SelectionDecision.java
+++ src/test/java/com/javafortesters/chap008/examples/SelectionDecision.java
@@ -24,12 +24,106 @@ public class SelectionDecision {
     public void catOrCatsTest(){
 
         Assert.assertEquals("cat", catOrCats(1));
+        System.out.println("onecat " + catOrCats(1));
         Assert.assertEquals("cats", catOrCats(2));
+        System.out.println("twocat " + catOrCats(2));
     }
 
     public String catOrCats(int numberOfCats){
+
         String output;
 
-        numberOfCats > 1 ? output = "cats" : output ="cat";
+        output = numberOfCats > 1 ? "cats" : "cat";
+
+        return output;
+    }
+
+    @Test
+    public void ifAddHttp(){
+
+        String url = "www.dsl.sk";
+
+        if(!url.startsWith("http://")){
+            url = addHttp(url);
+        }
+
+        Assert.assertEquals("http://www.dsl.sk", url);
+    }
+
+    @Test
+    public void ifExercise(){
+        boolean truthey = true;
+
+        if(truthey == true){
+            Assert.assertTrue(truthey);
+            Assert.assertFalse(!truthey);
+        }
+    }
+
+    @Test
+    public void ifElseAddHttp(){
+
+        String url = "www.dsl.sk";
+
+        if(url.startsWith("http://")){
+        }
+
+        else{
+            url = addHttp(url);
+        }
+
+        Assert.assertTrue(url.startsWith("http://"));
+        Assert.assertEquals("http://www.dsl.sk", url);
+    }
+
+    @Test
+    public void ifElseExercize(){
+
+        boolean truthy = true;
+
+        if(truthy == true) {
+            Assert.assertTrue(truthy);
+            Assert.assertFalse(!truthy);
+        }
+        else Assert.assertFalse(truthy);
+
+    }
+
+    @Test
+    public void nestedIfElseExercise(){
+
+        boolean truthy = false;
+        boolean falsey =true;
+
+        if(truthy == true){
+            if(falsey == false){
+                if(truthy == true && falsey == false){
+                    if(truthy == true || falsey == true){
+                        Assert.assertTrue(truthy);
+                        Assert.assertFalse(falsey);
+                        System.out.println("truthy is " + truthy + " and falsey is " + falsey);
+                    }
+                }
+            }
+            else {
+                Assert.assertTrue(truthy);
+                Assert.assertTrue(falsey);
+                System.out.println("truthy is " + truthy + " and falsey is " + falsey);
+            }
+        }
+        else{
+            if(truthy == false){
+                if(falsey == true){
+                    Assert.assertFalse(truthy);
+                    Assert.assertTrue(falsey);
+                    System.out.println("truthy is " + truthy + " and falsey is " + falsey);
+                }
+                else{
+                    Assert.assertFalse(truthy);
+                    Assert.assertFalse(falsey);
+                    System.out.println("truthy is " + truthy + " and falsey is " + falsey);
+                }
+            }
+        }
     }
 }
