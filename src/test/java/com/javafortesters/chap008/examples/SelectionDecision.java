a616471191196c99c62770377489f2dd4f64a873
cszalai <cszalai@davincisoftware.sk>
2017-02-23 10:13:41 +0100

    final changes for chapter 8

--- src/test/java/com/javafortesters/chap008/examples/SelectionDecision.java
+++ src/test/java/com/javafortesters/chap008/examples/SelectionDecision.java
@@ -1,5 +1,6 @@
 package com.javafortesters.chap008.examples;
 
+// import com.sun.java.util.jar.pack.Instruction;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -126,4 +127,127 @@ public class SelectionDecision {
             }
         }
     }
+
+    @Test
+    public void switchExample(){
+        Assert.assertEquals("M", likelyGenderIs("sir"));
+        Assert.assertEquals("M", likelyGenderIs("mr"));
+        Assert.assertEquals("M", likelyGenderIs("master"));
+        Assert.assertEquals("F", likelyGenderIs("miss"));
+        Assert.assertEquals("F", likelyGenderIs("mrs"));
+        Assert.assertEquals("F", likelyGenderIs("ms"));
+        Assert.assertEquals("F", likelyGenderIs("lady"));
+        Assert.assertEquals("F", likelyGenderIs("madam"));
+    }
+
+    public String likelyGenderIs(String title){
+
+        String likelyGender;
+
+        switch (title.toLowerCase()){
+            case "sir":
+                likelyGender = "M";
+                break;
+            case "mr":
+                likelyGender = "M";
+                break;
+            case "master":
+                likelyGender = "M";
+                break;
+            default:
+                likelyGender = "F";
+        }
+
+        return likelyGender;
+    }
+
+    @Test
+    public void switchExerciseShortCode(){
+        Assert.assertEquals("United Kingdom", longName("UK"));
+        Assert.assertEquals("United States", longName("US"));
+        Assert.assertEquals("United States", longName("USA"));
+        Assert.assertEquals("France", longName("FR"));
+        Assert.assertEquals("Sweden", longName("SE"));
+        Assert.assertEquals("Rest Of World", longName("SVK"));
+        Assert.assertEquals("Rest Of World", longName("Us"));
+        Assert.assertEquals("United Kingdom", longName("uK"));
+        Assert.assertEquals("United Kingdom", longName("Uk"));
+        Assert.assertEquals("United Kingdom", longName("uk"));
+    }
+
+    public String longName(String shortCode){
+
+        String fullname;
+
+        switch (shortCode){
+            case "UK":
+            case "Uk":
+            case "uK":
+            case "uk":
+                fullname = "United Kingdom";
+                break;
+            case "US":
+            case "USA":
+                fullname = "United States";
+                break;
+            case "FR":
+                fullname = "France";
+                break;
+            case "SE":
+                fullname = "Sweden";
+                break;
+            default:
+                fullname = "Rest Of World";
+                break;
+        }
+
+        return fullname;
+
+    }
+
+    @Test
+    public void switchExerciseInt(){
+        Assert.assertEquals("One", intToString(1));
+        Assert.assertEquals("Two", intToString(2));
+        Assert.assertEquals("Three", intToString(3));
+        Assert.assertEquals("Four", intToString(4));
+        Assert.assertEquals("Too big", intToString(5));
+        Assert.assertEquals("Too small", intToString(0));
+    }
+
+    public String intToString(int num){
+
+        String str = null;
+
+        if(num > 4){
+            num = 5;
+        }else if(num < 1){
+            num = 0;
+        }
+
+        switch (num){
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+
+                str = "Four";
+                break;
+            case 5:
+                str = "Too big";
+                break;
+            case 0:
+                str = "Too small";
+                break;
+        }
+
+        return str;
+    }
+
 }
